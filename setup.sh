aeb69171b90bee9a45de617de744293b64903678
SweetyKekS <orbvenom@gmail.com>
2025-06-28 12:46:48 +0300

    git commit -m '4th change'

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 # выполнить для загрузки всех необходимых библиотек
 
-```bash
 #!/bin/bash
 
+echo "Устанавливаю зависимости из requirements.txt..."
 pip install -r requirements.txt
+
+echo "Загружаю модель spaCy для английского..."
 python -m spacy download en_core_web_sm
+
+echo "Загружаю модель spaCy для русского..."
 python -m spacy download ru_core_news_sm
+
+echo "Установка завершена."
